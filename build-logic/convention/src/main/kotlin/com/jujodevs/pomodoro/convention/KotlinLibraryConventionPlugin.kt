a9869dba24ca22c88b4d7040d27709bd8244a089
Juan José Bonillo <jujo.bonillo90@gmail.com>
2026-03-22 11:19:36 +0100

    Build: migrate to AGP 9 built-in Kotlin (#23)
    * build: migrate to AGP 9 built-in Kotlin
    
    * build: align detekt compose config

--- build-logic/convention/src/main/kotlin/com/jujodevs/pomodoro/convention/KotlinLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/jujodevs/pomodoro/convention/KotlinLibraryConventionPlugin.kt
@@ -5,9 +5,7 @@ import com.jujodevs.pomodoro.convention.extensions.configureKotlin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
 class KotlinLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -17,10 +15,7 @@ class KotlinLibraryConventionPlugin : Plugin<Project> {
             }
 
             configureJava()
-
-            extensions.configure<KotlinProjectExtension> {
-                configureKotlin(this)
-            }
+            configureKotlin()
 
             tasks.withType<Test>().configureEach {
                 failOnNoDiscoveredTests.set(false)
